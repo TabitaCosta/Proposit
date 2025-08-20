4d19e954d6e5f0b2fe087b5b4231bac4b0245e92
Tabita Costa <tabitacostagc@gmail.com>
2025-08-19 23:10:38 -0300

    feat: cria as rotas

--- proposit/routes/web.php
+++ proposit/routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ProposalController;
+use App\Http\Controllers\RoleController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('events.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Eventos
+Route::resource('events', EventController::class);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Propostas
+Route::resource('proposals', ProposalController::class);
 
-require __DIR__.'/auth.php';
+// Papéis
+Route::resource('roles', RoleController::class);
